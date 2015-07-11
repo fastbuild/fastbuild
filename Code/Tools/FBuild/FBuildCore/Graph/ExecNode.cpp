94a5bee2ad57e3aabd925c88142df1173e751f1e
inbilla <inbilla@gmail.com>
2015-07-11 21:06:02 +1000

    Fixing preallocation amount of static dependencies for ExecNode

--- Code/Tools/FBuild/FBuildCore/Graph/ExecNode.cpp
+++ Code/Tools/FBuild/FBuildCore/Graph/ExecNode.cpp
@@ -34,7 +34,7 @@ ExecNode::ExecNode( const AString & dstFileName,
 , m_ExpectedReturnCode( expectedReturnCode )
 {
 	ASSERT( executable );
-	m_StaticDependencies.SetCapacity( 2 );
+	m_StaticDependencies.SetCapacity( m_InputFiles.GetSize() + 1 );
 	m_StaticDependencies.Append(m_InputFiles);
 	m_StaticDependencies.Append( Dependency( executable ) );
 	m_Type = EXEC_NODE;
