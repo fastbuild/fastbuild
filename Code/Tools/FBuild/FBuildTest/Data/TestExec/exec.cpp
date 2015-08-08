887336a3d6c0b9396d894484b609ef6e25748a92
inbilla <inbilla@gmail.com>
2015-08-09 02:32:33 +1000

    Adding unit tests for Exec commands. Unit tests include some basic usage cases. Also testing of the new UseStdout flag.

---
+++ Code/Tools/FBuild/FBuildTest/Data/TestExec/exec.cpp
@@ -0,0 +1,32 @@
+//
+// An simple executable to run
+//
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, char *argv[], char *envp[])
+{
+	// Touch each file listed
+	for (int i = 1; i < argc; ++i)
+	{
+		const char * arg = argv[i];
+
+		// Make a new filename based on the input
+		std::string outFileName = arg;
+		outFileName += ".out";
+
+		// Touch the file
+		std::ofstream file;
+		file.open(outFileName);
+		file << "T";
+		file.close();
+
+		// Generate some output
+		// on STDOUT based on the arguments too
+		std::cout << "Touched: " << outFileName << std::endl;
+	}
+	
+	return argc - 1;
+}
+
