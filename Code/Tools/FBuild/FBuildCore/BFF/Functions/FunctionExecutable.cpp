5bec70f6ef555a518f975b3f54e8fe1ee8d9616b
Layla <layla@laylamah.com>
2016-07-31 15:07:39 -0400

    + Added hack to make UE4 link with .lib files which are specified as DLL nodes using lib.exe - ( allow DLL nodes to depend on EXE nodes )
    Signed-off-by: Layla <layla@laylamah.com>

--- Code/Tools/FBuild/FBuildCore/BFF/Functions/FunctionExecutable.cpp
+++ Code/Tools/FBuild/FBuildCore/BFF/Functions/FunctionExecutable.cpp
@@ -250,6 +250,15 @@ bool FunctionExecutable::DependOnNode( const BFFIterator & iter, Node * node, De
 {
 	ASSERT( node );
 
+	// !! HACK: INSIGHTFUL CHANGE FOR UE4: an external executable ?
+	if (node->GetType() == Node::EXE_NODE)
+	{
+		// depend on node - will use exe output at build time
+		nodes.Append(Dependency(node));
+		return true;
+	}
+	// !! END HACK: INSIGHTFUL CHANGE FOR UE4: an external executable ?
+	
 	// a previously declared library?
 	if ( node->GetType() == Node::LIBRARY_NODE )
 	{
