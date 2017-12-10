0521c392bdad25c755777fa52ba5cc75db522c70
ffulin <franta.fulin@gmail.com>
2017-12-09 18:21:25 -0800

    [Improvement] Support WindRiver compiler (dcc.exe) (treat as gcc) (Thanks to Mohamed El-Shahawi)

--- Code/Tools/FBuild/FBuildCore/Graph/CompilerNode.cpp
+++ Code/Tools/FBuild/FBuildCore/Graph/CompilerNode.cpp
@@ -139,7 +139,9 @@ bool CompilerNode::InitializeCompilerFamily( const BFFIterator & iter, const Fun
         if ( compiler.EndsWithI( "gcc.exe" ) ||
              compiler.EndsWithI( "gcc" ) ||
              compiler.EndsWithI( "g++.exe" ) ||
-             compiler.EndsWithI( "g++" ) )
+             compiler.EndsWithI( "g++" ) ||
+             compiler.EndsWithI( "dcc.exe" ) || // WindRiver
+             compiler.EndsWithI( "dcc" ) )      // WindRiver
         {
             m_CompilerFamilyEnum = GCC;
             return true;
