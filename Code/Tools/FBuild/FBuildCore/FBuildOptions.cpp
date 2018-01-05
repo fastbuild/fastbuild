71b1bfc98b5f15ec572fd987a25b4a5270aeb407
Stas Cymbalov <dummyunit@gmail.com>
2018-01-05 16:50:59 +0300

    By default show progress bar only if stdout goes to the terminal
    This allows to redirect FBuild output to file or pager (like 'less')
    without needing to specify '-noprogress' to get a readable result.
    
    Also added option '-progress' to force the old behaviour -- to show
    progress bar unconditionally.
    
    Windows implementation of redirect detection supports both native
    console windows and Cygwin/MSYS terminals.

--- Code/Tools/FBuild/FBuildCore/FBuildOptions.cpp
+++ Code/Tools/FBuild/FBuildCore/FBuildOptions.cpp
@@ -55,6 +55,8 @@ FBuildOptions::OptionsResult FBuildOptions::ProcessCommandLine( int argc, char *
         }
     }
 
+    bool progressOptionSpecified = false;
+
     // Parse options
     for ( int32_t i=1; i<argc; ++i ) // start from 1 to skip exe name
     {
@@ -181,6 +183,13 @@ FBuildOptions::OptionsResult FBuildOptions::ProcessCommandLine( int argc, char *
             else if ( thisArg == "-noprogress" )
             {
                 m_ShowProgress = false;
+                progressOptionSpecified = true;
+                continue;
+            }
+            else if ( thisArg == "-progress" )
+            {
+                m_ShowProgress = true;
+                progressOptionSpecified = true;
                 continue;
             }
             else if ( thisArg == "-nostoponerror")
@@ -241,6 +250,7 @@ FBuildOptions::OptionsResult FBuildOptions::ProcessCommandLine( int argc, char *
             else if ( ( thisArg == "-ide" ) || ( thisArg == "-vs" ) )
             {
                 m_ShowProgress = false;
+                progressOptionSpecified = true;
                 #if defined( __WINDOWS__ )
                     m_FixupErrorPaths = true;
                     m_WrapperMode = WRAPPER_MODE_MAIN_PROCESS;
@@ -295,6 +305,12 @@ FBuildOptions::OptionsResult FBuildOptions::ProcessCommandLine( int argc, char *
             // assume target
             m_Targets.Append( thisArg );
         }
+    }
+
+    if ( progressOptionSpecified == false )
+    {
+        // By default show progress bar only if stdout goes to the terminal
+        m_ShowProgress = ( Env::IsStdOutRedirected() == false );
     }
 
     // Default to build "all"
@@ -456,6 +472,7 @@ void FBuildOptions::DisplayHelp( const AString & programName ) const
             " -j[x]          Explicitly set LOCAL worker thread count X, instead of\n"
             "                default of hardware thread count.\n"
             " -noprogress    Don't show the progress bar while building.\n"
+            " -progress      Show the progress bar while building, even if stdout is redirected.\n"
             " -nostoponerror Don't stop building on first error. Try to build as much\n"
             "                as possible.\n"
             " -nosummaryonerror Hide the summary if the build fails. Implies -summary.\n"
