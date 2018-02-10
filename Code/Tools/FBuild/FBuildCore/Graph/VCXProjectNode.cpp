faa80e1e30cecf603e8bc6690cbc3a42c00c9c95
James Leitch <jleitch@riotgames.com>
2018-02-09 17:36:05 -0800

    GUID generation based on full path.

--- Code/Tools/FBuild/FBuildCore/Graph/VCXProjectNode.cpp
+++ Code/Tools/FBuild/FBuildCore/Graph/VCXProjectNode.cpp
@@ -63,6 +63,12 @@ VCXProjectNode::VCXProjectNode( const AString & projectOutput,
 
     // depend on the input nodes
     m_StaticDependencies.Append( paths );
+
+    // generate GUID if not specified
+    if ( m_ProjectGuid.IsEmpty() )
+    {
+        VSProjectGenerator::FormatDeterministicProjectGUID( m_ProjectGuid, m_Name );
+    }
 }
 
 // DESTRUCTOR
@@ -75,12 +81,6 @@ VCXProjectNode::~VCXProjectNode() = default;
 {
     VSProjectGenerator pg;
     pg.SetBasePaths( m_ProjectBasePaths );
-
-    // get project file name only
-    const char * p1 = m_Name.FindLast( NATIVE_SLASH );
-    p1 = p1 ? p1 : m_Name.Get();
-    AStackString<> projectName( p1 );
-    pg.SetProjectName( projectName );
 
     // Globals
     pg.SetRootNamespace( m_RootNamespace );
