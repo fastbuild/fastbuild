917802c2eff1c9c5c5e4d8ca59069b788e8400ce
ffulin <franta.fulin@gmail.com>
2018-11-02 18:52:06 -0700

    Post-integration cleanup: Process termination cleanup
     - whitespace style

--- Code/Core/Process/Process.h
+++ Code/Core/Process/Process.h
@@ -49,11 +49,10 @@ public:
     static uint32_t GetCurrentId();
 private:
     #if defined( __WINDOWS__ )
-        void KillProcessTreeInternal(
-            const void * hProc,  // HANDLE
-            const uint32_t processID,
-            const uint64_t processCreationTime );
-        static uint64_t GetProcessCreationTime( const void * hProc );  // HANDLE
+        void KillProcessTreeInternal( const void * hProc, // HANDLE
+                                      const uint32_t processID,
+                                      const uint64_t processCreationTime );
+        static uint64_t GetProcessCreationTime( const void * hProc ); // HANDLE
         void Read( void * handle, AutoPtr< char > & buffer, uint32_t & sizeSoFar, uint32_t & bufferSize );
         char * Read( void * handle, uint32_t * bytesRead );
         uint32_t Read( void * handle, char * outputBuffer, uint32_t outputBufferSize );
