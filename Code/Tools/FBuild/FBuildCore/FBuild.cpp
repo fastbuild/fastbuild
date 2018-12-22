ff129283d661754fb73b1c902dfb96e00171085c
ffulin <franta.fulin@gmail.com>
2018-12-21 21:34:25 -0800

    Tidy up GetCacheFileName function:
     - replace generic param names with descriptive ones

--- Code/Tools/FBuild/FBuildCore/FBuild.cpp
+++ Code/Tools/FBuild/FBuildCore/FBuild.cpp
@@ -665,13 +665,22 @@ void FBuild::UpdateBuildStatus( const Node * node )
 
 // GetCacheFileName
 //------------------------------------------------------------------------------
-void FBuild::GetCacheFileName( uint64_t keyA, uint32_t keyB, uint64_t keyC, uint64_t keyD, AString & path ) const
+void FBuild::GetCacheFileName( uint64_t preprocessedSourceKey,
+                               uint32_t commandLineKey,
+                               uint64_t toolChainKey,
+                               uint64_t pchKey,
+                               AString & path ) const
 {
     // cache version - bump if cache format is changed
     static const int cacheVersion( 9 );
 
     // format example: 2377DE32AB045A2D_FED872A1_AB62FEAA23498AAC-32A2B04375A2D7DE.7
-    path.Format( "%016" PRIX64 "_%08X_%016" PRIX64 "-%016" PRIX64 ".%u", keyA, keyB, keyC, keyD, cacheVersion );
+    path.Format( "%016" PRIX64 "_%08X_%016" PRIX64 "-%016" PRIX64 ".%u",
+                 preprocessedSourceKey,
+                 commandLineKey,
+                 toolChainKey,
+                 pchKey,
+                 cacheVersion );
 }
 
 // DisplayTargetList
--- Code/Tools/FBuild/FBuildCore/Graph/ObjectNode.cpp
+++ Code/Tools/FBuild/FBuildCore/Graph/ObjectNode.cpp
@@ -1054,33 +1054,33 @@ const AString & ObjectNode::GetCacheName( Job * job ) const
 
     // hash the pre-processed intput data
     ASSERT( job->GetData() );
-    uint64_t a = xxHash::Calc64( job->GetData(), job->GetDataSize() );
+    const uint64_t preprocessedSourceKey = xxHash::Calc64( job->GetData(), job->GetDataSize() );
 
     // hash the build "environment"
     // TODO:B Exclude preprocessor control defines (the preprocessed input has considered those already)
-    uint32_t b;
+    uint32_t commandLineKey;
     {
         Args args;
         const bool useDeoptimization = false;
         const bool showIncludes = false;
         const bool finalize = false; // Don't write args to reponse file
         BuildArgs( job, args, PASS_COMPILE_PREPROCESSED, useDeoptimization, showIncludes, finalize );
-        b = xxHash::Calc32( args.GetRawArgs().Get(), args.GetRawArgs().GetLength() );
+        commandLineKey = xxHash::Calc32( args.GetRawArgs().Get(), args.GetRawArgs().GetLength() );
     }
 
     // ToolChain hash
-    uint64_t c = GetCompiler()->CastTo< CompilerNode >()->GetManifest().GetToolId();
+    const uint64_t toolChainKey = GetCompiler()->CastTo< CompilerNode >()->GetManifest().GetToolId();
 
     // PCH dependency
-    uint64_t d = 0;
+    uint64_t pchKey = 0;
     if ( GetFlag( FLAG_USING_PCH ) && GetFlag( FLAG_MSVC ) )
     {
-        d = GetPrecompiledHeader()->m_PCHCacheKey;
-        ASSERT( d != 0 ); // Should not be in here if PCH is not cached
+        pchKey = GetPrecompiledHeader()->m_PCHCacheKey;
+        ASSERT( pchKey != 0 ); // Should not be in here if PCH is not cached
     }
 
     AStackString<> cacheName;
-    FBuild::Get().GetCacheFileName( a, b, c, d, cacheName );
+    FBuild::Get().GetCacheFileName( preprocessedSourceKey, commandLineKey, toolChainKey, pchKey, cacheName );
     job->SetCacheName(cacheName);
 
     return job->GetCacheName();
