ecd1dcd3bfba2884ac74a2c55cde1686927168ff
Nick Edwards <nedwards@sumo-digital.com>
2019-01-21 09:55:06 +0000

    Use wordexp to tokenize argument string on Linux/OSX. Should perform shell-like expansion of the args string to more reliably produce a correct argv/argc.

--- Code/Core/Process/Process.cpp
+++ Code/Core/Process/Process.cpp
@@ -31,6 +31,7 @@
     #include <string.h>
     #include <sys/wait.h>
     #include <unistd.h>
+    #include <wordexp.h>
 #endif
 
 // Static Data
@@ -299,21 +300,25 @@ bool Process::Spawn( const char * executable,
         if ( args )
         {
             // Tokenize
-            AStackString<> argCopy( args );
-            argCopy.Tokenize( splitArgs );
+            wordexp_t expResult;
+            memset(&expResult, 0, sizeof(wordexp_t));
 
-            // Build Vector
-            for ( auto & arg : splitArgs )
+            int wordRes = wordexp( args, &expResult, 0 );
+            if (wordRes != 0)
+                return false;
+
+            char** words = expResult.we_wordv;
+            unsigned int wordCount = expResult.we_wordc;
+            for ( unsigned int wordIndex = 0; wordIndex < wordCount; ++wordIndex )
             {
-                if ( arg.BeginsWith( '"' ) && arg.EndsWith( '"' ) )
-                {
-                    // strip quotes
-                    arg.SetLength( arg.GetLength() - 1 ); // trim end quote
-                    argVector.Append( arg.Get() + 1 ); // skip start quote
-                    continue;
-                }
-                argVector.Append( arg.Get() ); // leave arg as-is
+                // Make a copy of the string and stick it in splitArgs, as the strings allocated
+                // by wordexp will be freed when we leave this scope.
+                splitArgs.Append( AString( words[wordIndex] ) );
+                AString& argString = splitArgs.Top();
+                argVector.Append( argString.Get() );
             }
+
+            wordfree( &expResult );
         }
         argVector.Append( nullptr ); // argv must have be nullptr terminated
 
