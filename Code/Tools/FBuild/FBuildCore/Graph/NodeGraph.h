ee1b8e926713f20409ce23626195ccafaf7e25a6
ffulin <franta.fulin@gmail.com>
2019-03-03 16:40:21 -0800

    Post-integration: Add basic documentation for new .CompilerOutputKeepBaseExtentsion

--- Code/Tools/FBuild/FBuildCore/Graph/NodeGraph.h
+++ Code/Tools/FBuild/FBuildCore/Graph/NodeGraph.h
@@ -55,7 +55,7 @@ public:
     }
     inline ~NodeGraphHeader() = default;
 
-    enum { NODE_GRAPH_CURRENT_VERSION = 122 };
+    enum { NODE_GRAPH_CURRENT_VERSION = 123 };
 
     bool IsValid() const
     {
