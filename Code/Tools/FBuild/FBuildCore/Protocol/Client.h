64d28cb9715597eb3963a098304e05311f1cecaa
Brian Burgin <brian.burgin@ni.com>
2019-05-23 15:05:58 -0500

    Remove redundant worker list

--- Code/Tools/FBuild/FBuildCore/Protocol/Client.h
+++ Code/Tools/FBuild/FBuildCore/Protocol/Client.h
@@ -124,7 +124,6 @@ private:
     float            m_WorkerListRefreshElapsedTimeSec;  // seconds since we started the worker list retries
     int32_t          m_WorkerListRefreshLimitSec;        // max seconds elapsed until stop retrying worker list
     int32_t          m_WorkerConnectionRetryLimitSec;    // max seconds elapsed until stop retrying connection to worker
-    Array< AString >    m_WorkerList;   // workers to connect to
     volatile bool       m_ShouldExit;   // signal from main thread
     volatile bool       m_Exited;         // flagged on exit
     bool                m_DetailedLogging;
