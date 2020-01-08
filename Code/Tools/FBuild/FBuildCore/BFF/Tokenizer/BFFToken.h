0b3ecc9d6c95434c263349859d27618b4b10f901
Francisco Facioni <francisco.facioni@hawkeyeinnovations.com>
2020-01-08 16:28:20 +0000

    Fixes MSVC compilation with 14.24.28314

--- Code/Tools/FBuild/FBuildCore/BFF/Tokenizer/BFFToken.h
+++ Code/Tools/FBuild/FBuildCore/BFF/Tokenizer/BFFToken.h
@@ -44,6 +44,8 @@ public:
     BFFToken( const BFFFile & file, const char * sourcePos, BFFTokenType type, const AString & stringValue );
     BFFToken( const BFFFile & file, const char * sourcePos, BFFTokenType type, const bool boolValue );
 
+    BFFToken& operator=(const BFFToken&) = delete;
+
     // Convenience wrappers
     bool IsInvalid() const                          { return ( m_Type == BFFTokenType::Invalid ); }
     bool IsIdentifier() const                       { return ( m_Type == BFFTokenType::Identifier ); }
