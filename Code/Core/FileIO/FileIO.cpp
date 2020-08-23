ec75d77d650de343b9ecceaf40376f9d993e9411
ffulin <franta.fulin@gmail.com>
2020-08-22 18:18:22 -0700

    Improve output on failing assertion

--- Code/Core/FileIO/FileIO.cpp
+++ Code/Core/FileIO/FileIO.cpp
@@ -7,6 +7,7 @@
 #include "FileStream.h"
 
 // Core
+#include "Core/Env/ErrorFormat.h"
 #include "Core/FileIO/PathUtils.h"
 #include "Core/Process/Thread.h"
 #include "Core/Profile/Profile.h"
@@ -1395,7 +1396,7 @@
             // timeout so we don't get stuck in here forever
             if ( timer.GetElapsed() > (float)timeoutSeconds )
             {
-                ASSERT( false && "WorkAroundForWindowsFilePermissionProblem Failed!" );
+                ASSERTM( false, "WorkAroundForWindowsFilePermissionProblem Failed: %s (timeout %u s)", LAST_ERROR_STR, timeoutSeconds );
                 return;
             }
         }
