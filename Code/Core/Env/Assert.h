ee6e3fee6cf9011ca21cfea43a65fa046f31468c
dummyunit <dummyunit@gmail.com>
2021-07-07 06:42:40 +0300

    Allow to manually enable asserts in non-debug builds (#852)
    * Some code assumed that ASSERTS_ENABLED is defined when DEBUG is
      defined and vice versa. This made it difficult to temporary enable
      asserts in non-debug builds by defining ASSERTS_ENABLED as such code
      started to produce warnings and/or errors.
    * Always use __builtin_trap() to break into debugger on Linux.
      __builtin_trap() generates ud2 instruction on x86. Both ud2 and int3
      stop the program execution when the program is run under gdb, but
      execution of ud2 generates SIGILL instead of SIGTRAP and the latter
      isn't intercepted by libFuzzer runtime. So with int3 asserts just
      terminate BFFFuzzer instead of handling the crash (saving the input,
      displaying stack trace and restarting fuzzing).

--- Code/Core/Env/Assert.h
+++ Code/Core/Env/Assert.h
@@ -10,27 +10,25 @@
 //------------------------------------------------------------------------------
 #if defined( __WINDOWS__ )
     #define BREAK_IN_DEBUGGER __debugbreak()
-#elif defined( __APPLE__ )
+#elif defined( __LINUX__ ) || defined( __APPLE__ )
     #define BREAK_IN_DEBUGGER __builtin_trap()
-#elif defined( __LINUX__ )
-    #if defined( __X64__ )
-        #define BREAK_IN_DEBUGGER __asm__ __volatile__("int $3")
-    #else
-        #define BREAK_IN_DEBUGGER __builtin_trap()
-    #endif
 #else
     #error Unknown platform
+#endif
+
+#if !defined( DEBUG ) && !defined( RELEASE )
+    #error neither DEBUG nor RELEASE were defined
 #endif
 
 // Global functions
 //------------------------------------------------------------------------------
 bool IsDebuggerAttached();
 
-// DEBUG
-//------------------------------------------------------------------------------
 #ifdef DEBUG
     #define ASSERTS_ENABLED
+#endif
 
+#ifdef ASSERTS_ENABLED
     // Create a no-return helper to improve static analysis
     #if defined( __WINDOWS__ )
         __declspec(noreturn) void NoReturn();
@@ -96,10 +94,7 @@ bool IsDebuggerAttached();
 
         static AssertCallback * s_AssertCallback;
     };
-
-// RELEASE
-//------------------------------------------------------------------------------
-#elif defined ( RELEASE )
+#else
     #define ASSERT( expression )            \
         do {                                \
         PRAGMA_DISABLE_PUSH_MSVC(4127)      \
@@ -118,8 +113,6 @@ bool IsDebuggerAttached();
         PRAGMA_DISABLE_PUSH_MSVC(4127)      \
         } while ( false )                   \
         PRAGMA_DISABLE_POP_MSVC
-#else
-    #error neither DEBUG nor RELEASE were defined
 #endif
 
 //------------------------------------------------------------------------------
