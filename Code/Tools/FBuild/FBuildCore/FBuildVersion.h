e99916f257260c65ff03a80d7bad26f4eadc50b3
ffulin <11589403+ffulin@users.noreply.github.com>
2022-04-10 17:49:39 +0930

    v1.06

--- Code/Tools/FBuild/FBuildCore/FBuildVersion.h
+++ Code/Tools/FBuild/FBuildCore/FBuildVersion.h
@@ -4,8 +4,8 @@
 
 // Defines
 //------------------------------------------------------------------------------
-#define FBUILD_VERSION_STRING "v1.05"
-#define FBUILD_VERSION (uint32_t)105
+#define FBUILD_VERSION_STRING "v1.06"
+#define FBUILD_VERSION (uint32_t)106
 #if defined( __WINDOWS__ )
     #define FBUILD_VERSION_PLATFORM "Windows"
 #elif defined( __APPLE__ )
