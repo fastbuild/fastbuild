291474baa8826d97380af21b281fa1de8ee74860
Stas Cymbalov <dummyunit@gmail.com>
2022-06-29 17:28:46 +0300

    Fix warnings when building in Clang 14 on Linux
    Clang 14 started to emit -Wunused-value for the left side of the comma
    operator even in constexpr/template contexts.
    This change in behavior breaks the build via this warning:
    ./Core/Containers/Forward.h:11:84: error: left operand of comma operator has no effect [-Werror,-Wunused-value]
    template < class T, class U > struct ForwardType< T &, U >      { static_assert( ( sizeof( T ), false ), "can not forward rvalue as lvalue" ); };

--- Code/Core/Containers/Forward.h
+++ Code/Core/Containers/Forward.h
@@ -8,7 +8,7 @@ template < class T, class U > struct ForwardType< T &, U & >    { using type = T
     __pragma( clang diagnostic push )
     __pragma( clang diagnostic ignored "-Wcomma" )
 #endif
-template < class T, class U > struct ForwardType< T &, U >      { static_assert( ( sizeof( T ), false ), "can not forward rvalue as lvalue" ); };
+template < class T, class U > struct ForwardType< T &, U >      { static_assert( ( (void)sizeof( T ), false ), "can not forward rvalue as lvalue" ); };
 #if defined( __WINDOWS__) && defined( __clang__ )
     __pragma( clang diagnostic pop )
 #endif
