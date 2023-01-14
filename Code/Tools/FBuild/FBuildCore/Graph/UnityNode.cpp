b28bcc40eecc27cf9ac6fb2fc995ac2bc855c027
ffulin <11589403+ffulin@users.noreply.github.com>
2023-01-14 17:04:41 +1030

    Fix unused variable warning
    * caught by Clang 15.0.6

--- Code/Tools/FBuild/FBuildCore/Graph/UnityNode.cpp
+++ Code/Tools/FBuild/FBuildCore/Graph/UnityNode.cpp
@@ -330,7 +330,9 @@ UnityNode::~UnityNode()
     const float numFilesPerUnity = (float)numFiles / (float)m_NumUnityFilesToCreate;
     float remainingInThisUnity( 0.0 );
 
-    uint32_t numFilesWritten( 0 );
+    #if defined(ASSERTS_ENABLED)
+        uint32_t numFilesWritten( 0 );
+    #endif
 
     size_t index = 0;
 
@@ -421,7 +423,9 @@ UnityNode::~UnityNode()
 
             // count the file, whether we wrote it or not, to keep unity files stable
             index++;
-            numFilesWritten++;
+            #if defined(ASSERTS_ENABLED)
+                numFilesWritten++;
+            #endif
         }
 
         // write allocation of includes for this unity file
