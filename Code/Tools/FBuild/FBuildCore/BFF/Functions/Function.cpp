e66d23d2d14ff69fcdf26333eb8158fc8f7ffa71
ffulin <11589403+ffulin@users.noreply.github.com>
2025-04-12 10:01:02 +0930

    Fix "leak" of g_Functions
     - not an actual leak, but rather asymetrical alloc/free

--- Code/Tools/FBuild/FBuildCore/BFF/Functions/Function.cpp
+++ Code/Tools/FBuild/FBuildCore/BFF/Functions/Function.cpp
@@ -61,7 +61,7 @@
 
 // Static
 //------------------------------------------------------------------------------
-/*static*/ Array<const Function *> g_Functions;
+/*static*/ StackArray<const Function *> g_Functions;
 
 // CONSTRUCTOR
 //------------------------------------------------------------------------------
@@ -94,7 +94,6 @@ Function::~Function() = default;
 //------------------------------------------------------------------------------
 /*static*/ void Function::Create()
 {
-    g_Functions.SetCapacity( 25 );
     g_Functions.Append( FNEW( FunctionAlias ) );
     g_Functions.Append( FNEW( FunctionCompiler ) );
     g_Functions.Append( FNEW( FunctionCopy ) );
