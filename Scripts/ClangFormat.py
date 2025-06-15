797aa9e6af938c43fbe02139c1e956248f8cca07
ffulin <11589403+ffulin@users.noreply.github.com>
2025-06-15 11:15:41 +0930

    ClangFormat: Improve clang format helper script
     - auto-detect clang-format in github CI compatible way
     - check that the clang-format found is at least v19, which is required by settings we use and fixes we rely on
     - add a "--check" arg, which when True will report if formatting is needed (and fail overall) for use in the CI. Without this arg, fixes will be applied to files to allow bulk updates to styling.

--- Scripts/ClangFormat.py
+++ Scripts/ClangFormat.py
@@ -7,6 +7,7 @@
 
 # imports
 #-------------------------------------------------------------------------------
+import argparse
 import os
 import subprocess
 
@@ -26,6 +27,65 @@ def get_files_recurse(base_path, out_files):
 
             out_files.append(os.path.join(root, file))
 
+# get_clang_format_exe
+#-------------------------------------------------------------------------------
+def get_clang_format_exe():
+    # TODO:B Extend to OSX and Linux
+    potential_locations = [
+                            '..\\External\\SDK\\Clang\\Windows\\20.1.0\\bin\\clang-format.exe',
+                            'C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\VC\\Tools\\Llvm\\bin\\clang-format.exe',
+                            'C:\\Program Files\\Microsoft Visual Studio\\2022\\Enterprise\\VC\\Tools\\Llvm\\bin\\clang-format.exe',
+                            'C:\\Program Files\\LLVM\\bin\\clang-format.exe',
+                          ]
+
+    # We require at least clang-format 19.x.x
+    min_major_version = 19
+
+    too_old_versions = ''
+
+    for potential_location in potential_locations:
+        if os.path.exists(potential_location):
+            # found at this location. Check version
+            cmdline = '"{}" --version"'.format(potential_location)
+            result = subprocess.run(cmdline,
+                                    shell = True,
+                                    capture_output = True,
+                                    text = True)
+
+            # handle invocation failures
+            if result.returncode != 0:
+                print('FAILED to check version for clang-format: "{}"'.format(potential_location))
+                continue
+
+            # check major version
+            version_string = result.stdout
+            version_string = version_string.replace('clang-format version ', '')
+            major_version = int(version_string.split('.')[0])
+            if major_version < min_major_version:
+                # version is too old
+                too_old_versions += 'Too old:\n'
+                too_old_versions += ' - Location: {}\n'.format(potential_location)
+                too_old_versions += ' - Version : {}\n'.format(version_string)
+                continue;
+
+            print('Using clang-format:')
+            print(' - Location: {}'.format(potential_location))
+            print(' - Version : {}'.format(version_string))
+            return potential_location
+
+    if too_old_versions == '':
+        print('Failed to find any clang-format executable')
+    else:
+        print('Failed to find clang-format of at least v{}.x.x'.format(min_major_version))
+        print(too_old_versions)
+    exit(2);
+
+# Handle args
+#-------------------------------------------------------------------------------
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--check', action='store_true', help='Do not apply fixes. Return failure if issues found.')
+args = parser.parse_args()
+
 # Get all the files
 #-------------------------------------------------------------------------------
 print('Getting file lists...')
@@ -34,28 +94,62 @@ get_files_recurse(os.path.join("..", "Code"), files)
 print('...Found {} files'.format(len(files)))
 print('')
 
+# Find Clang Format executable
 #-------------------------------------------------------------------------------
-print('Checking...')
+clang_format_exe = get_clang_format_exe()
+
+#-------------------------------------------------------------------------------
+if args.check:
+    print('Checking...')
+else:
+    print('Formatting...')
 num_files_needing_changes = 0
 ok = True
 for file in files:
     # check if this file needs changes
-    args = "..\\External\\SDK\\Clang\\Windows\\20.1.0\\bin\\clang-format.exe --dry-run {}".format(file)
-    result = subprocess.run(args, shell = True, capture_output = True, text = True)
+    cmdline = '"{}" --dry-run "{}"'.format(clang_format_exe, file)
+    result = subprocess.run(cmdline,
+                            shell = True,
+                            capture_output = True,
+                            text = True)
 
-    #
+    # handle invocation failures
     if result.returncode != 0:
-        print('FAILED to check {}'.format(file))
+        print('FAILED to run clang-format on {}'.format(file))
         ok = False
         continue
 
-    # print info for files that need changes
+    # does file need changes?
     if len(result.stderr) != 0:
-        print('{}'.format(result.stderr))
-        ok = False
         num_files_needing_changes = num_files_needing_changes + 1
 
+        if args.check:
+            # in "check" mode, finding needed changes is an error
+            ok = False
+
+            # print details of the necessary fixes
+            print('{}'.format(result.stderr))
+        else:
+            # apply fixes
+            cmdline = '"{}" --i "{}"'.format(clang_format_exe, file)
+            result = subprocess.run(cmdline,
+                                    shell = True,
+                                    capture_output = True,
+                                    text = True)
+
+            # handle invocation failures
+            if result.returncode != 0:
+                print('FAILED to run clang-format on {}'.format(file))
+                ok = False
+                continue
+
+            # note that we applied fixes to the file
+            print('Fixed: {}'.format(file))
+
 # summarize overall work
-print('.. {} files need changes.'.format(num_files_needing_changes))
+if args.check:
+    print('.. {} files need changes.'.format(num_files_needing_changes))
+else:
+    print('.. {} files formatted.'.format(num_files_needing_changes))
 
 exit(0 if ok else 1)
