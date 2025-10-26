61eafeaa6733b681061ed8f0a56b488356e3e12c
ffulin <11589403+ffulin@users.noreply.github.com>
2025-10-26 11:52:57 +1030

    xxHash API Improvements:
     - Expose dynamic dispatch functionality for x86_64
     - Expose Calc32 convenience function
    
    No change in behavior in this commit, but this unlocks use of AVX2 for large hashing operations with xxHash3, and will make it simpler to migrate old uses of xxHash to xxHash3.
    
    Example timings (Ryzen 7950X3D):
    Big - cached:
     - xxHash-32       : 0.001s @  7.985 GiB/s (hash: 0x4a3157be)
     - xxHash-64       : 0.001s @ 13.398 GiB/s (hash: 0148e8533cf39b81)
     - xxHash3-64      : 0.000s @ 43.192 GiB/s (hash: b6e214b5af71b466)
     - xxHash3-64 Dyn  : 0.000s @ 69.605 GiB/s (hash: b6e214b5af71b466)
    Big - uncached:
     - xxHash-32       : 0.103s @  9.666 GiB/s (hash: 0xe9f5d474)
     - xxHash-64       : 0.089s @ 11.219 GiB/s (hash: 9f2d18e0c8e8b641)
     - xxHash3-64      : 0.025s @ 39.359 GiB/s (hash: 8b023611f354df52)
     - xxHash3-64 Dyn  : 0.021s @ 46.541 GiB/s (hash: 8b023611f354df52)
    Small - not cached:
     - xxHash-32       : 0.197s @  1.644 GiB/s (hash: 0x3431cdee)
     - xxHash-64       : 0.221s @  1.464 GiB/s (hash: 8e3128524da66e4a)
     - xxHash3-64      : 0.142s @  2.283 GiB/s (hash: c314f0605f6f3f9e)
     - xxHash3-64 Dyn  : 0.145s @  2.238 GiB/s (hash: c314f0605f6f3f9e)
    Small - cached:
     - xxHash-32       : 0.001s @  3.317 GiB/s (hash: 0x85d759e6)
     - xxHash-64       : 0.001s @  3.306 GiB/s (hash: 9675be091b949f4a)
     - xxHash3-64      : 0.000s @  9.042 GiB/s (hash: 64e670120b152776)
     - xxHash3-64 Dyn  : 0.000s @  8.959 GiB/s (hash: 64e670120b152776)

--- Code/Core/Math/xxHash.h
+++ Code/Core/Math/xxHash.h
@@ -16,9 +16,11 @@ unsigned long long XXH64( const void * input, size_t length, unsigned long long 
 
 // xxhash3
 unsigned long long xxHashLib_XXH3_64bits( const void * input, size_t length );
+uint64_t XXH3_64bits_dispatch( const void * input, size_t length );
 struct xxHashLib_XXH3_state_s;
 void xxHashLib_XXH3_64bits_reset( xxHashLib_XXH3_state_s * state );
 void xxHashLib_XXH3_64bits_update( xxHashLib_XXH3_state_s * state, const void * data, size_t dataSize );
+void XXH3_64bits_update_dispatch( xxHashLib_XXH3_state_s * state, const void * data, size_t dataSize );
 uint64_t xxHashLib_XXH3_64bits_digest( xxHashLib_XXH3_state_s * state );
 };
 
@@ -42,9 +44,37 @@ private:
 class xxHash3
 {
 public:
-    static uint64_t Calc64( const void * buffer, size_t len );
+    // Functions for small inputs, such as strings
+    //  - Avoid dynamic dispatch where available as it's slower for short inputs
+    static uint32_t Calc32( const void * buffer, size_t len ) { return static_cast<uint32_t>( Calc64Static( buffer, len ) ); }
+    static uint32_t Calc32( const AString & string ) { return static_cast<uint32_t>( Calc64Static( string.Get(), string.GetLength() ) ); }
+    static uint64_t Calc64( const void * buffer, size_t len ) { return Calc64Static( buffer, len ); }
+    static uint64_t Calc64( const AString & string ) { return Calc64Static( string.Get(), string.GetLength() ); }
 
-    static uint64_t Calc64( const AString & string ) { return Calc64( string.Get(), string.GetLength() ); }
+    // Functions for large inputs
+    //  - Use dynamic dispatch on x86_64 to utilize AVX2 etc if available
+    //  - On ARM, dynamic dispatch is not needed
+#if defined( __x86_64__ ) || defined( _M_X64 )
+    #define CALC_FUNC Calc64Dynamic
+#else
+    #define CALC_FUNC Calc64Static
+#endif
+    static uint32_t Calc32Big( const void * buffer, size_t len ) { return static_cast<uint32_t>( CALC_FUNC( buffer, len ) ); }
+    static uint32_t Calc32Big( const AString & string ) { return static_cast<uint32_t>( CALC_FUNC( string.Get(), string.GetLength() ) ); }
+    static uint64_t Calc64Big( const void * buffer, size_t len ) { return CALC_FUNC( buffer, len ); }
+    static uint64_t Calc64Big( const AString & buffer ) { return CALC_FUNC( buffer.Get(), buffer.GetLength() ); }
+#undef CALC_FUNC
+
+protected:
+    // Explicitly use static dispatch (use compiled architecture functionality only)
+    static uint64_t Calc64Static( const void * buffer, size_t len ) { return xxHashLib_XXH3_64bits( buffer, len ); }
+    static uint64_t Calc64Static( const AString & string ) { return xxHashLib_XXH3_64bits( string.Get(), string.GetLength() ); }
+
+#if defined( __x86_64__ ) || defined( _M_X64 )
+    // Explicitly use dynamic dispatch (utilize AVX2 etc if available)
+    static uint64_t Calc64Dynamic( const void * buffer, size_t len ) { return XXH3_64bits_dispatch( buffer, len ); }
+    static uint64_t Calc64Dynamic( const AString & string ) { return XXH3_64bits_dispatch( string.Get(), string.GetLength() ); }
+#endif
 };
 
 // xxHash3Accumulator
@@ -69,7 +99,17 @@ public:
                                       data,
                                       dataSize );
     }
-
+    void AddDataBig( const void * data, size_t dataSize )
+    {
+        ASSERT( mFinalized == false );
+#if defined( __x86_64__ ) || defined( _M_X64 )
+        XXH3_64bits_update_dispatch( reinterpret_cast<xxHashLib_XXH3_state_s *>( m_State ),
+#else
+        xxHashLib_XXH3_64bits_update( reinterpret_cast<xxHashLib_XXH3_state_s *>( m_State ),
+#endif
+                                     data,
+                                     dataSize );
+    }
     // Generate the final hash once and get the result
     uint64_t Finalize64()
     {
@@ -102,13 +142,6 @@ PRAGMA_DISABLE_POP_MSVC
 /*static*/ inline uint64_t xxHash::Calc64( const void * buffer, size_t len )
 {
     return XXH64( buffer, len, kXXHashSeed );
-}
-
-// Calc64 (xxHash3)
-//------------------------------------------------------------------------------
-/*static*/ inline uint64_t xxHash3::Calc64( const void * buffer, size_t len )
-{
-    return xxHashLib_XXH3_64bits( buffer, len );
 }
 
 //------------------------------------------------------------------------------
