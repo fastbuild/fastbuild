123a26281809b933faa64e71085ae7615e7d5639
ffulin <11589403+ffulin@users.noreply.github.com>
2025-11-29 14:08:36 +1030

    v1.18

--- Code/Tools/FBuild/FBuildCore/FBuildVersion.h
+++ Code/Tools/FBuild/FBuildCore/FBuildVersion.h
@@ -6,7 +6,7 @@
 //------------------------------------------------------------------------------
 // FASTBuild controlled versions
 #define FBUILD_VERSION_MAJOR 1
-#define FBUILD_VERSION_MINOR 17
+#define FBUILD_VERSION_MINOR 18
 
 // Version extensions reserved for 3rd party use
 #if !defined( FBUILD_VERSION_EXT )
