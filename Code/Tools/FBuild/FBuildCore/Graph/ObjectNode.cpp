2529ccc25cb46dbf3bf83d49fba58d6e9db204e3
ffulin <11589403+ffulin@users.noreply.github.com>
2025-12-06 17:10:14 +1030

    [Fix] Fix precompiled headers not rebuilding after PCHOptions edited (v1.18 regression) (#1134)
    - Store a hash of the PCHOptions on the ObjectNode for the precompiled header so that rebuilds will trigger when it changes
    [Note] DB version changed

--- Code/Tools/FBuild/FBuildCore/Graph/ObjectNode.cpp
+++ Code/Tools/FBuild/FBuildCore/Graph/ObjectNode.cpp
@@ -75,6 +75,7 @@ REFLECT_NODE_BEGIN( ObjectNode, Node, MetaNone() )
     REFLECT( m_PreprocessorFlags.m_Flags,           "PreprocessorFlags",                MetaHidden() )
     REFLECT( m_PCHCacheKey,                         "PCHCacheKey",                      MetaHidden() + MetaIgnoreForComparison() )
     REFLECT( m_OwnerObjectList,                     "OwnerObjectList",                  MetaHidden() )
+    REFLECT( m_PCHOptionsHash,                      "PCHOptionsHash",                   MetaHidden() )
 REFLECT_END( ObjectNode )
 
 // CONSTRUCTOR
@@ -91,6 +92,7 @@ ObjectNode::ObjectNode()
 /*virtual*/ bool ObjectNode::Initialize( NodeGraph & nodeGraph, const BFFToken * iter, const Function * function )
 {
     ASSERT( m_OwnerObjectList ); // Must be set before we get here
+    ASSERT( !IsCreatingPCH() || m_PCHOptionsHash );
 
     // .PreBuildDependencies (OwnerObjectList will have handled checks/errors)
     m_PreBuildDependencies = m_OwnerObjectList->GetPreBuildDependencies();
