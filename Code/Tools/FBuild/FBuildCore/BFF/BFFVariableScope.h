6470f7c858364eb8429eb17ea6af7ef76a816885
Dan Andrus <danieleandrus@gmail.com>
2026-03-25 16:59:33 +0100

    Add BFFVariableScope

---
+++ Code/Tools/FBuild/FBuildCore/BFF/BFFVariableScope.h
@@ -0,0 +1,15 @@
+// BFFVariableScope.h - the collection of variables within a single scope or struct
+//------------------------------------------------------------------------------
+#pragma once
+
+// Includes
+//------------------------------------------------------------------------------
+#include "Tools/FBuild/FBuildCore/BFF/BFFVariable.h"
+
+// Core
+#include "Core/Containers/UnorderedMap.h"
+#include "Core/Strings/AString.h"
+
+// BFFVariableScope
+//------------------------------------------------------------------------------
+using BFFVariableScope = UnorderedMap<const AString, BFFVariable>;
